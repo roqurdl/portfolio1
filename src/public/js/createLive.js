10b9777b29a961d40586759bc59d81d6694a167d
roqurdl <uyhnri@gmail.com>
2022-11-15 23:30:43 +0900

    camera and audio List error

--- src/public/js/createLive.js
+++ src/public/js/createLive.js
@@ -1,13 +1,103 @@
 const video = document.querySelector(`video`);
 const startBtn = document.querySelector(`#startBtn`);
+const muteBtn = document.querySelector(`#mute`);
+const cameraBtn = document.querySelector(`#camera`);
+const cameraList = document.querySelector(`#cameras`);
+const audioList = document.querySelector(`#audios`);
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true,
+let Stream;
+let muted = false;
+let camera = false;
+
+async function getCameras() {
+  try {
+    /**
+     * device.kind = audio or vided
+     * device.label = divce name
+     * device.deviceId = unique numver of device
+     */
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    const currentCamera = Stream.getVideoTracks()[0];
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
+      cameraList.appendChild(option);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function getAudios() {
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  const audios = devices.filter((device) => device.kind === "audioinput");
+  const currentAudio = Stream.getAudioTracks()[0];
+  audios.forEach((audio) => {
+    const option = document.querySelector("option");
+    option.value = audio.id;
+    option.innerText = audio.label;
+    if (currentAudio.label === audio.label) {
+      option.selected = true;
+    }
+    audioList.appendChild(option);
   });
-  video.srcObject = stream;
+}
+
+async function init() {
+  try {
+    Stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  await getCameras();
+  // await getAudios();
+  video.srcObject = Stream;
   video.play();
-};
+}
+init();
+
+async function handleStart() {
+  startBtn.innerText = "Stop";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+  const recorder = new MediaRecorder(Stream);
+}
+
+function handleMute() {
+  Stream.getAudioTracks().forEach((track) => (track.enabled = !track.enabled));
+  if (!muted) {
+    muteBtn.innerText = "Unmute";
+    muted = true;
+  } else {
+    muteBtn.innerText = "Mute";
+    muted = false;
+  }
+}
+function handleCamera() {
+  Stream.getVideoTracks().forEach((track) => (track.enabled = !track.enabled));
+  if (!camera) {
+    cameraBtn.innerText = "Camera ON";
+    camera = true;
+  } else {
+    cameraBtn.innerText = "Camera OFF";
+    camera = false;
+  }
+}
+
+function handleStop() {
+  startBtn.innerText = "Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleStart);
+}
 
 startBtn.addEventListener(`click`, handleStart);
+muteBtn.addEventListener(`click`, handleMute);
+cameraBtn.addEventListener(`click`, handleCamera);
